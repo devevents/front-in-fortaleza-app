56bdd9ff6fd367e139e3e9212de606ce93c16404
Marcelo Silva <marcelosilva.developer@gmail.com>
2015-05-17 12:37:32 -0300

    Add Photos Module

---
+++ www/app/photos/photos.js
@@ -0,0 +1,9 @@
+'use strict';
+
+angular.module('confboilerplate.photos', [
+  'confboilerplate.photos.controllers',
+  'confboilerplate.photos.services'
+]);
+
+angular.module('confboilerplate.photos.controllers', []);
+angular.module('confboilerplate.photos.services', []);
