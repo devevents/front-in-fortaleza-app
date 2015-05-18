6475abb02e98a22ae0c69d0ee7bb4acd77510d09
Marcelo Silva <marcelosilva.developer@gmail.com>
2015-05-18 00:45:12 -0300

    Implements getMedia() /media/{id}

--- www/app/photos/photos-service.js
+++ www/app/photos/photos-service.js
@@ -1,4 +1,38 @@
 angular.module('confboilerplate.photos.services')
 
 .service('Photos', function ($http) {
+
+	var config = {
+		url: 'https://api.instagram.com/v1',
+		client_id: '9f02a975012347d99daf55b3507b4f07',
+		count: 20
+	};
+
+	return {
+		fetch: function(url) {
+			url = url || config.url + '/tags/frontinfortaleza/media/recent';
+			var params = {
+				'params': {
+					'client_id': config.client_id,
+					'count': config.count,
+					'callback': 'JSON_CALLBACK'
+				}
+			};
+
+			return $http.jsonp(url, params);
+		},
+
+		getMedia: function(id) {
+			var url = config.url + '/media/' + id;
+			var params = {
+				'params': {
+					'client_id': config.client_id,
+					'count': config.count,
+					'callback': 'JSON_CALLBACK'
+				}
+			};
+
+			return $http.jsonp(url, params);
+		}
+	};
 });
